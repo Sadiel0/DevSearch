efcbc5918bb61946721afe0bc9eaa032a2c5bd60
Sadiel0 <sadielalmanza@outlook.com>
2022-05-02 01:43:54 +0000

    userProfile

--- users/views.py
+++ users/views.py
@@ -6,3 +6,14 @@ def profiles(request):
     profiles = Profile.objects.all()
     context = {'profiles':profiles}
     return render(request, 'users/profile.html', context)
+
+def userProfile(request, pk):
+    profile = Profile.objects.get(id = pk)
+
+    topSkills = profile.skill_set.exclude(description__exact="")
+    otherSkills = profile.skill_set.filter(description = "")
+
+
+    context = {'profile':profile, 'topSkills':topSkills, 'otherSkills':otherSkills}
+
+    return render(request, 'users/user-profile.html', context)
