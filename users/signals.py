078cb5c1d3157ac176da4ac098553d5f53ff6ca2
Sadiel0 <sadielalmanza@outlook.com>
2022-05-10 02:49:59 +0000

    finished registation form

--- users/signals.py
+++ users/signals.py
@@ -5,7 +5,7 @@ from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
 
-@receiver(post_save, sender=Profile)
+# @receiver(post_save, sender=Profile)
 def createProfile(sender, instance , created,  **kwargs):
     print('Triggering profile creation')
     if created :
--- users/views.py
+++ users/views.py
@@ -3,8 +3,8 @@ from django.contrib.auth.models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import login, authenticate, logout
 from django.contrib import messages
-from django.contrib.auth.forms import UserCreationForm
 from .models import Profile
+from .forms import CustomUserCreationForm
 
 # Create your views here.
 def loginUser(request):
@@ -38,10 +38,10 @@ def logoutUser(request):
     return redirect('login')
 
 def registerUser(request):
-    form = UserCreationForm()
+    form = CustomUserCreationForm()
     page = 'register'
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form = CustomUserCreationForm(request.POST)
         if form.is_valid():
             user = form.save(commit = False)
             user.username = user.username.lower()
